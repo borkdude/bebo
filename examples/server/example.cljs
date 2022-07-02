0b23efa1ed7f78380465012d514b3eb31c8813be
Michiel Borkent <michielborkent@gmail.com>
2022-07-02 21:10:29 +0200

    example

--- examples/server/example.cljs
+++ examples/server/example.cljs
@@ -5,7 +5,7 @@
 (def port 8080)
 
 (defn handler [req]
-  (let [agent (-> req .-headers (.get "user-agent"))
+  (let [agent (-> req (.-headers) (.get "user-agent"))
         body (str "Your user agent is: " (or agent
                                              "Unknown"))]
     (new js/Response body #js {:status 200})))
